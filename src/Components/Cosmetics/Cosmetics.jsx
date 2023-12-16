d2ca858f922419e07b58d8bea04ffa5ad02ee956
Rohul1 <rohul669319@gmail.com>
2023-12-16 13:19:32 +0600

    updated

--- src/Components/Cosmetics/Cosmetics.jsx
+++ src/Components/Cosmetics/Cosmetics.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import add from '../../Utilities/Calculate';
+
 
 const Cosmetics = () => {
-
-    const first = 55;
-    const second = 60;
-    const total = add
+const cosmetics = [
+    {id : 1, name : "alta", price : 100},
+    {id : 2, name : "curi", price : 200},
+    {id : 3, name : "nak ful", price : 300},
+    {id : 4, name : "tip", price : 400}
+]
+    
     return (
         <div>
             <h2>Welcome to my cosmetic store!!</h2>
-            <p>Total : {total}</p>
+            {
+                cosmetics.map(cosmetic => console.log(cosmetic))
+            }
+         
         </div>
     );
 };
