e8129330385621cc568de78e041bce848d855e2e
Rohul1 <rohul669319@gmail.com>
2023-12-16 14:02:01 +0600

    added json

--- src/Components/Cosmetics/Cosmetics.jsx
+++ src/Components/Cosmetics/Cosmetics.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic';
 
 
 const Cosmetics = () => {
-const cosmetics = [
-    {id : 1, name : "alta", price : 100},
-    {id : 2, name : "curi", price : 200},
-    {id : 3, name : "nak ful", price : 300},
-    {id : 4, name : "tip", price : 400}
-]
+const [cosmetics, setCosmetics] = useState([]);
+
+useEffect(()=>{
+    fetch("data.json")
+    .then(res =>res.json())
+    .then(data => setCosmetics(data))
+},[])
     
     return (
         <div>
             <h2>Welcome to my cosmetic store!!</h2>
             {
-                cosmetics.map(cosmetic => <Cosmetic key={cosmetic.id}></Cosmetic>)
+                cosmetics.map(cosmetic => <Cosmetic key={cosmetic.id} cosmetic={cosmetic}></Cosmetic>)
             }
          
         </div>
