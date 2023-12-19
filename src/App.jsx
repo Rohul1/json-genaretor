928af89a6b2cd2ca6ee8f354f8aa6c5a9a372d2f
Rohul1 <rohul669319@gmail.com>
2023-12-19 12:00:12 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Cosmetics from './Components/Cosmetics/Cosmetics'
-import Handle from './Components/Handle/Handle'
+
 
 function App() {
  
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <Cosmetics></Cosmetics>
-      <Handle></Handle>
+     
     </div>
   )
 }
--- src/Components/Cosmetic/Cosmetic.jsx
+++ src/Components/Cosmetic/Cosmetic.jsx
@@ -5,16 +5,16 @@ const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
 
     const addToCart = (id) =>{
-        console.log("item added", id)
+        localStorage.setItem(id, 1)
     }
     const addToCartWithParam = () => addToCart(id)
     return (
         <div className='product'>
-            <h2>Buy this: {name} </h2>
+            <h2 className='bbbb'>Buy this: {name} </h2>
             <p>Only for: ${price}</p>
             <p><small>Id: {id}</small></p>
             <button onClick={addToCartWithParam}>Add to cart</button>
-            <button onClick={()=> addToCart(id)}>Purchase</button>
+            
         </div>
     );
 };
